c2d3acd4c7c183588e1c95f9bc09414563387fd6
oozsen <oozsen@gmail.com>
2020-12-04 15:57:11 +0300

    * OrientationPage.xaml: OrientationPage eklendi
    * OrientationPage.xaml.cs:
    
    * Monkey.png: Monkey.png embedded resource olarak eklendi
    
    * ImageResourceExtension.cs: Embedded resource için extension class ı
      eklendi

---
+++ VisualStateManagerDemo/OrientationPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VisualStateManagerDemo
+{
+    public partial class OrientationPage : ContentPage
+    {
+        public OrientationPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            var state = (width > height) ? "Landscape" : "Portrait";
+            VisualStateManager.GoToState(DetailsBoxView, state);
+            VisualStateManager.GoToState(AdditionalText, state);
+        }
+    }
+}
